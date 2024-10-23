085992ce089b9db3e85bcaaa81360288b760c886
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-10-23 18:11:20 -0400

    added the console examples and added the module in src

--- Primer/src/main.ts
+++ Primer/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Name, WeatherLocation } from "./modules/NameAndWeather";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));  // for example purposes for 80 - 92 pages in the book
+
+  let name = new Name("Adam", "Freeman"); 
+  let loc = new WeatherLocation("raining", "London"); 
+
+  console.log(name.nameMessage); 
+  console.log(loc.weatherMessage);
